380458d178217c09bd517f5b34f71a5b256e9486
beninburley <130599399+beninburley@users.noreply.github.com>
2024-12-03 08:59:26 -0700

    Removing Certs to ensure website can run

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d http://group2-7turtleshelterv3.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email benjaminahansen17@gmail.com
