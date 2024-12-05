fb2d0df9acb8b4ebda651161f17a5875a6d11130
beninburley <130599399+beninburley@users.noreply.github.com>
2024-12-04 18:59:24 -0700

    More HTTPS attempts. using --expand

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d group2-7turtleshelterv3.us-east-1.elasticbeanstalk.com -d group27.is404.net --nginx --agree-tos --email benjaminahansen17@gmail.com
+
+# Request or expand certificate for both domains
+sudo certbot -n --expand -d group2-7turtleshelterv3.us-east-1.elasticbeanstalk.com -d group27.is404.net --nginx --agree-tos --email benjaminahansen17@gmail.com
