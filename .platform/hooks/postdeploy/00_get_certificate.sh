db4f7fa5ec9601dffa952e8f514c476007ac0e13
beninburley <130599399+beninburley@users.noreply.github.com>
2024-12-04 18:52:00 -0700

    Co-authored-by: hoopesjake <hoopesjake@users.noreply.github.com> Attempting DNS HTTPS

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d group2-7turtleshelterv3.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email benjaminahansen17@gmail.com
+sudo certbot -n -d group2-7turtleshelterv3.us-east-1.elasticbeanstalk.com -d group27.is404.net --nginx --agree-tos --email benjaminahansen17@gmail.com
